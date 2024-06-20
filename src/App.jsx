1dd3f571a7b95e4287c2db10f4998e80dd8e974a
NikitaSalnykov <salnikov.nkt@gmail.com>
2024-06-20 17:49:22 +0300

    update actions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import AboutPage from './pages/AboutPage/AboutPage';
 const test = import.meta.env.VITE_API_TEST;
 
 function App() {
+  
   console.log(test);
+
   return (
     <div>
       <Routes>
