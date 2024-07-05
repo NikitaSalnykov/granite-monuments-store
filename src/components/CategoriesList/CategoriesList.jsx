2c883f713721795304f16a8fe9efe20ed3b1ed13
NikitaSalnykov <salnikov.nkt@gmail.com>
2024-07-05 12:11:55 +0300

    Update CategoriesList.jsx

--- src/components/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesList/CategoriesList.jsx
@@ -10,13 +10,14 @@ export const CategoriesList = ({categories, sm}) => {
       <ul className={`grid place-items-end ${sm ? "grid-cols-2 " : "grid-cols-1"} lg:px-[60px] md:grid md:grid-cols-2 lg:grid-cols-3 gap-3`}>
         {categories.map((el, index) => (
           <li key={index}>
-            <Link to={el.link} className="relative rounded overflow w-full grayscale-[0%] hover:shadow-xl  bg-black over:bg-opacity-60 transition cursor-pointer">
+<Link to={el.link} >
+<div className="relative rounded overflow w-full grayscale-[0%] hover:shadow-xl  bg-black over:bg-opacity-60 transition cursor-pointer">
               <img className='object-cover w-full h-full brightness-[50%] hover:brightness-[80%]' src={el.img} alt="Hanging Planters" />
               <p
                 className={`absolute text-white top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2  text-center ${sm ? "text-xs md:text-lg" : "text-2xl"}`}>
                 {el.title}
               </p>
-            </Link>
+            </div></Link>
           </li>
         ))}
       </ul>
