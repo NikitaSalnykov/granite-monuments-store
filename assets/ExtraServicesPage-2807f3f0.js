ecd9f1e4e71d6da1e49eccfa8251fb81eb5bae30
NikitaSalnykov <121830017+NikitaSalnykov@users.noreply.github.com>
2024-07-09 15:03:33 +0000

    Deploying to gh-pages from @ NikitaSalnykov/granite-monuments-store@f91879d42631b2ca2c3284bc2dc1c6b0b0729b3d 🚀

--- assets/ExtraServicesPage-2807f3f0.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as s,T as i}from"./index-92b0b337.js";const a=()=>{const{t:e}=t();return t(),s.jsxs("div",{children:[s.jsx(i,{title:e("extra_services"),description:e("landscaping-description")}),s.jsx("div",{className:"h-screen"})]})};export{a as default};
---
+++ assets/FavoritePage-ed8b5f11.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-8a094c6d.js";const t=()=>e.jsx("div",{children:"FavoritePage"});export{t as default};
