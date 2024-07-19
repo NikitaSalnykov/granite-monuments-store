6d51a41adcf7e36b7fe4b9aeeeb1ac31ba29e952
NikitaSalnykov <121830017+NikitaSalnykov@users.noreply.github.com>
2024-07-19 15:28:43 +0000

    Deploying to gh-pages from @ NikitaSalnykov/granite-monuments-store@3f08945e34cad1156af5345b1a665d4ade110b40 🚀

--- assets/FavoritePage-183b263b.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-4fee1b7c.js";const t=()=>e.jsx("div",{children:"FavoritePage"});export{t as default};
---
+++ assets/FavoritePage-9256e759.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-7f2eb446.js";const t=()=>e.jsx("div",{children:"FavoritePage"});export{t as default};
